7400cf5194051c76576866eccea61872b55cf258
victor-arcuri <victor@arcuri.dev>
2026-04-13 14:18:16 -0300

    feat(metricas): adiciona cabeçalho inicial para o namespace de Métricas

---
+++ include/Metricas.hpp
@@ -0,0 +1,10 @@
+#ifndef METRICAS_HPP
+#define METRICAS_HPP
+namespace Metricas {
+	double calcular_RET(double cotacao_final, double cotacao_inicial);
+	double calcular_retorno_elementar(double cotacao_atual, double cotacao_anterior);
+	double calcular_AVGRET(double w, double* cotacoes);
+	double calcular_STAB(double w, double* cotacoes);
+	double calcular_CONS(double w, double* cotacoes);
+};
+#endif
