77e7a491a6da91a49c025c833c9544fb6d0b3499
victor-arcuri <victor@arcuri.dev>
2026-04-14 11:10:11 -0300

    feat(cliente): adiciona a base do TAD cliente, cabeçalho e implementação da classe e métodos de compra e venda de ações

---
+++ include/Cliente.hpp
@@ -0,0 +1,19 @@
+#ifndef CLIENTE_HPP
+#define CLIENTE_HPP
+
+#include "Acao.hpp"
+#include "VetorDinamico.hpp"
+
+class Cliente {
+	private:
+		int id;
+		VetorDinamico<Acao*> carteira;
+	
+	public:
+		Cliente(int id);
+		void comprar_acao(Acao* acao);
+		void vender_acao(Acao* acao);
+
+};
+
+#endif
---
+++ src/Cliente.cpp
@@ -0,0 +1,14 @@
+#include "Cliente.hpp"
+
+Cliente::Cliente(int id): id(id), carteira(10){}
+void Cliente::comprar_acao(Acao* acao){
+	this->carteira.adicionar(acao);
+}
+void Cliente::vender_acao(Acao* acao){
+	for(int i = 0; i < this->carteira.get_tamanho(); i++){
+		if (this->carteira.obter(i)==acao){
+			this->carteira.remover_trocando(i);
+			return;
+		}
+	}
+}
