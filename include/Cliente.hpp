ece22a0ce1cc46336e0083482be7ca67ddf0704e
victor-arcuri <victor@arcuri.dev>
2026-04-17 22:09:22 -0300

    docs(cliente): comenta as classes e funcoes do cabecalho do TAD Cliente

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -4,6 +4,12 @@
 #include "Acao.hpp"
 #include "VetorDinamico.hpp"
 
+/** 
+ * Classe Cliente
+ * ---------------------------------------------------
+ * Tipo de Dado Abstrato responsável por gerenciar os clientes da aplicação,
+ * guardando sua carteira de ações e definindo operações de compra e venda.
+*/
 class Cliente {
 	private:
 		int id;
@@ -11,9 +17,41 @@ class Cliente {
 	
 	public:
 		Cliente(int id);
+
+		/** 
+		 * Função comprar_acao
+		 * ---------------------------------------------------
+		 * Adiciona uma ação à carteira do cliente.
+		 * 
+		 * acao: ponteiro para a ação a ser comprada.
+		*/
 		void comprar_acao(Acao* acao);
+
+		/** 
+		 * Função vender_acao
+		 * ---------------------------------------------------
+		 * Remove uma ação da carteira do cliente.
+		 * 
+		 * acao: ponteiro para a ação a ser vendida.
+		*/
 		void vender_acao(Acao* acao);
+
+		/** 
+		 * Função get_id
+		 * ---------------------------------------------------
+		 * Retorna o id do cliente.
+		 * 
+		 * retorno: retorna um inteiro representando o id do cliente.
+		*/
 		int get_id();
+
+		/** 
+		 * Função get_acoes
+		 * ---------------------------------------------------
+		 * Retorna a carteira de ações do cliente.
+		 * 
+		 * retorno: retorna um ponteiro para a carteira de ações do cliente.
+		*/
 		VetorDinamico<Acao*>* get_acoes();
 };
 
